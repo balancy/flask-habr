03b64131dbdbf93b98c2d1c5569b20a7a53bbddc
balancy <balancy@gmail.com>
2021-05-11 10:58:20 +0200

    Make flask use config with environmental variables

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-1. Run db by `docker-compose up -d`
-
-[comment]: <> (2. Connect to db)
-2. Initialize db by `flask db init`   
-3. Make migration db by `flask db migrate -m 'Initial migration'`
-4. Upgrade db by `flask db upgrade` 
+flask db init
+flask db migrate -m "comment"
+flask db upgrade
--- habr/config.py
+++ habr/config.py
@@ -1,3 +1,7 @@
+import os
+from dotenv import load_dotenv
+
+
 class Config(object):
     """Base config, uses staging database server."""
     DEBUG = False
@@ -13,8 +17,14 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
+    load_dotenv()
+    username = os.getenv('POSTGRES_USER')
+    password = os.getenv('POSTGRES_PASSWORD')
+    db = os.getenv('POSTGRES_DB')
     SQLALCHEMY_DATABASE_URI = ('postgresql+psycopg2://'
-                               'balancy:balancy@localhost:5432/habr_db')
+                               f'{username}:{password}@localhost:5432/{db}')
+    print(SQLALCHEMY_DATABASE_URI)
 
 
 class TestingConfig(Config):
