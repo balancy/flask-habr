2fdf7ebf5369609ef6e743baee23d2f763c4af60
balancy <balancy@gmail.com>
2021-05-12 13:30:26 +0200

    Add registraton for new users

--- posts/app.py
+++ posts/app.py
@@ -4,8 +4,8 @@ from flask_migrate import Migrate
 from werkzeug.utils import redirect
 
 from posts import config
-from posts.forms import LoginForm
-from posts.models import Tag, User
+from posts.forms import LoginForm, RegisterForm
+from posts.models import User
 from posts.models.database import db
 from posts.utils import get_popular_tags
 from posts.views.habr import habr_app
@@ -26,6 +26,44 @@ login_manager.login_view = 'login'
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
+
+
+@app.route('/register', methods=['GET', 'POST'], endpoint='register')
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('habr_app.posts'))
+
+    title = "Регистрация"
+    popular_tags = get_popular_tags()
+    register_form = RegisterForm()
+
+    # if request.method == 'GET':
+
+    if register_form.validate_on_submit():
+        user = User.query.filter_by(username=register_form.username.data).first()
+        if user:
+            flash('Такой пользователь уже есть')
+            return redirect(url_for('register'))
+
+        user = User(username=register_form.username.data)
+        user.set_password(register_form.password.data)
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('Вы успешно зарегистрировались')
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        flash('Проверьте требования полей в форме.')
+
+    return render_template(
+        'register.html',
+        title=title,
+        tags=popular_tags,
+        form=register_form,
+        current_user=current_user,
+    )
 
 
 @app.route('/login', methods=['GET', 'POST'], endpoint='login')
--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import (BooleanField, StringField, PasswordField, SubmitField)
+from wtforms.validators import EqualTo, DataRequired, Length
 
 
 class LoginForm(FlaskForm):
@@ -20,4 +20,39 @@ class LoginForm(FlaskForm):
         'Запомнить меня',
         default=True,
         render_kw={"class": "form-check-input"},
+    )
+
+
+class RegisterForm(FlaskForm):
+    username = StringField(
+        'Имя пользователя',
+        validators=[
+            DataRequired(message="Поле должно быть заполнено"),
+            Length(
+                min=4, max=25,
+                message='Имя пользователя должно '
+                        'быть длиной от 4 до 25 символов'
+            ),
+        ],
+        render_kw={"class": "form-control mt-3"},
+    )
+    password = PasswordField(
+        'Пароль',
+        validators=[
+            DataRequired(message="Поле должно быть заполнено"),
+            EqualTo('confirm', message='Пароли должны совпадать'),
+            Length(
+                min=6,
+                message='Пароль должен быть длиной от 6 символов'
+            ),
+        ],
+        render_kw={"class": "form-control mt-3"},
+    )
+    confirm = PasswordField(
+        'Повторите пароль',
+        render_kw={"class": "form-control mt-3"},
+    )
+    submit = SubmitField(
+        'Отправить',
+        render_kw={"class": "btn btn-outline-dark mt-3"},
     )
